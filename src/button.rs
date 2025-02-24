3a0598662300ee4cafbf7123715e92b4c7f2ff19
Altyn777 <darzeeia@gmail.com>
2025-02-24 18:44:05 +0200

    sized button

--- src/button.rs
+++ src/button.rs
@@ -1,5 +1,5 @@
 use egui::{
-    Button, CentralPanel, Color32, Context, Pos2, RichText, Ui, Vec2, Widget
+    Button, CentralPanel, Color32, Context, Label, Pos2, RichText, Ui, Vec2, ViewportCommand, Widget
 };
 
 #[derive(Default)]
@@ -11,20 +11,22 @@ impl ButtonDemo {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Default::default()
     }
+    fn ui_counter(&mut self, ui: &mut Ui) {
+        ui.horizontal(|ui| {
+            if ui.add_sized([50., 30.], Button::new("-")).clicked() {
+                self.counter -= 1;
+            }
+
+            ui.add_sized([50., 30.], Label::new(self.counter.to_string()));
+
+            if ui.add_sized([50., 30.], Button::new("+")).clicked() {
+                self.counter += 1;
+            }
+        });
+    }
 }
 
 fn button_demo(ui: &mut Ui, counter: &mut i32,_frame: &mut eframe::Frame) {
-    ui.horizontal(|ui| {
-        if ui.button("-").clicked() {
-            *counter -= 1;
-        }
-
-        ui.label(counter.to_string());
-
-        if ui.button("+").clicked() {
-            *counter += 1;
-        }
-    });
     if ui.add(Button::new("Click me")).clicked() {
         println!("Click");
     }
@@ -45,18 +47,24 @@ fn button_demo(ui: &mut Ui, counter: &mut i32,_frame: &mut eframe::Frame) {
 impl eframe::App for ButtonDemo {
     fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
+            self.ui_counter(ui);
+            ui.add_space(10.0);
+            if ui.add_sized([165., 30.], Button::new("Quit")).clicked() {
+                ctx.send_viewport_cmd(ViewportCommand::Close);
+            }
             button_demo(ui,  &mut self.counter, frame);
         });
     }
 }
 
 fn main() -> Result<(), eframe::Error> {
+    let viewport = egui::ViewportBuilder {
+        position: Some(Pos2 { x: 0., y:0. }),
+        inner_size: Some(Vec2 { x: 200., y: 200. }),
+        ..Default::default()
+    };
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder {
-            position: Some(Pos2 { x: 0., y:0. }),
-            inner_size: Some(Vec2 { x: 200., y: 200. }),
-            ..Default::default()
-        },
+        viewport,
         ..Default::default()
     };
     eframe::run_native(
